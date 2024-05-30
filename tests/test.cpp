feb762a1157f691c62295587e3068bbe04eb8bcf
Samarin Ivan <94916980+divan2@users.noreply.github.com>
2024-05-30 10:39:54 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,3 +9,13 @@ TEST(CircleTest, Constructor) {
   Circle circle(5.0);
   EXPECT_EQ(circle.radius(), 5.0);
 }
+
+TEST(CircleTest1, Constructor) {
+  Circle circle(9.0);
+  EXPECT_EQ(circle.radius(), 9.0);
+}
+
+TEST(CircleTest2, Constructor) {
+  Circle circle(4.0);
+  EXPECT_EQ(circle.radius(), 4.0);
+}
