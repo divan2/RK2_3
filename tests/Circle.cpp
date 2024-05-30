4cb96c03e2270bd329478ecc515555ada8049400
divan2 <samaraivan2005@yandex.ru>
2024-05-30 10:21:57 +0300

    files

---
+++ tests/Circle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Circle.h"
+//#include /* some graphics library */
+
+void Circle::draw() const
+{
+   std::cout << "Circle draw" << std::endl;
+   // ... Implementing the logic for drawing a circle
+}
